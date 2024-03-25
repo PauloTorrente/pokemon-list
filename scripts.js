46e85b4b796e1cfec21262dde0414969c38a68a8
PauloTorrente <paulotorrentebr@gmail.com>
2024-03-25 14:09:15 +0100

    pokemon

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let gen1 = document.getElementById("gen-1")
+console.log(gen1)
+gen1.innerHTML = "Generasión pokimon"
+const pkmbackground = document.querySelector(".infocard-list");
+pkmbackground.style.background = "yellowgreen";
+console.log(document.title);
